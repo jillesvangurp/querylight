26eb05cf26f3b12e5405ede1ffe75fb1903fc514
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2020-08-17 09:20:18 +0200

    multiplatform, kotlin 1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,62 @@
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.3.72"
+    kotlin("multiplatform") version "1.4.0"
+    id("com.github.ben-manes.versions") version "0.28.0" // gradle dependencyUpdates -Drevision=release
+    id("org.jmailen.kotlinter") version "2.4.1"
+    id("maven-publish")
+
 }
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation(kotlin("stdlib-js"))
-    testImplementation(kotlin("test-common"))
-    testImplementation(kotlin("test-annotations-common"))
-    testImplementation("io.kotest:kotest-assertions-core:4.1.3")
+kotlin {
+    jvm()
+    js {
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+                implementation("io.kotest:kotest-assertions-core:4.1.3")
+            }
+        }
 
-    testImplementation(kotlin("test-js"))
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+
+            }
+        }
+
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+        
+    }
+
 }
 
-kotlin.target.nodejs { }
+kotlinter {
+    // run gradle formatKotlin to fix
+    ignoreFailures = true
+}
+
+val ktLint by tasks.creating(org.jmailen.gradle.kotlinter.tasks.LintTask::class) {
+    group = "verification"
+    source(files("src"))
+
+}
+
+val ktFormat by tasks.creating(org.jmailen.gradle.kotlinter.tasks.FormatTask::class) {
+    group = "formatting"
+    source(files("src"))
+}
