914a84fdee8a7b5e5304e660eef35dadfb1167d1
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2023-06-20 12:39:34 +0200

    add publish script from kt-search

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+set -e
 
 die () {
     echo >&2 "$@"
@@ -6,12 +7,20 @@ die () {
 }
 
 [ "$#" -eq 1 ] || die "1 argument required, $# provided"
-echo $1 | grep -E -q '^[0-9]+\.[0-9]+(\.[0-9]+)$' || die "Semantic Version argument required, $1 provided"
+echo $1 | grep -E -q '^[0-9]+\.[0-9]+(\.[0-9]+).*?$' || die "Semantic Version argument required, $1 provided"
+
+[[ -z $(git status -s) ]] || die "git status is not clean"
 
 export TAG=$1
+
+gradle -Pversion="$TAG" publish
+
+#echo "upload to gcloud"
+#gsutil -m rsync -r localRepo gs://mvn-public-tryformation/releases
+
+echo "tagging"
+git tag "$TAG"
+
 echo "publishing $TAG"
 
-git tag $TAG
-gradle  -Pgroup=com.github.jillesvangurp -Pversion=$TAG publish
-rsync -azp  localrepo/* jillesvangurpcom@ftp.jillesvangurp.com:/srv/home/jillesvangurpcom/domains/jillesvangurp.com/htdocs/www/maven
-git push origin $TAG
+git push --tags
